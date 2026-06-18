076b94ad05ab411f69bc36b2845c2f59314bd6cd
Kshitij Bhandari <70257821+kshitij9000@users.noreply.github.com>
2026-06-18 18:53:35 +0545

    Update main.py
    feat: add deposit function with input validation

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 # Text Based Slot Machine - TechWithTom - Learning Python Again
 
-
+def deposit():
+    """Collects deposit amount from user."""
+    while True:
+        # Reads a line from input and converts to string
+        amount = input("Enter deposit amount: ")
+        # isdigit returns False if non numeric characters exist including -ve no
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Deposit amount must be greater than zero.")
+        else:
+            print("Enter valid positive number.")
+            
+    return amount
+        
+            
+            
+            
+            
 
